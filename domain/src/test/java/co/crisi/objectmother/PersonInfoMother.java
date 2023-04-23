1ee1ac79e53f157f294c1b777005e9a612c40e0d
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-23 10:20:33 -0500

    Updated the object mother

--- domain/src/test/java/co/crisi/objectmother/PersonInfoMother.java
+++ domain/src/test/java/co/crisi/objectmother/PersonInfoMother.java
@@ -20,4 +20,51 @@ public class PersonInfoMother {
                 .build();
     }
 
+    public PersonInfo randomSonWithGrandParents() {
+        return PersonInfo.builder()
+                .name("Colonel Aureliano")
+                .father(PersonInfo.builder()
+                        .name("Jose Arcadio")
+                        .lastName("Buendia")
+                        .father(randomFatherFather())
+                        .mother(randomMotherFather())
+                        .build())
+                .mother(PersonInfo.builder()
+                        .name("Ursula")
+                        .lastName("Iguaran")
+                        .father(randomFatherMother())
+                        .mother(randomMotherMother())
+                        .build())
+                .build();
+    }
+
+    public PersonInfo randomFatherFather() {
+        return PersonInfo.builder()
+                .name("Arcadiano")
+                .lastName("Buendia")
+                .build();
+    }
+
+    public PersonInfo randomMotherFather() {
+        return PersonInfo.builder()
+                .name("Maria de los Santos")
+                .lastName("Buenaventura")
+                .build();
+    }
+
+    public PersonInfo randomFatherMother() {
+        return PersonInfo.builder()
+                .name("Neftaly")
+                .lastName("Iguarán")
+                .build();
+    }
+
+    public PersonInfo randomMotherMother() {
+        return PersonInfo.builder()
+                .name("Nicolasa")
+                .lastName("Carabalí")
+                .build();
+    }
+
+
 }
