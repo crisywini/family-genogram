7ec695eef6669ba9953040b6ee4922a9ed9b7931
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-23 22:30:46 -0500

    Added the api impl

--- domain/src/main/java/co/crisi/port/api/PersonServicePort.java
+++ domain/src/main/java/co/crisi/port/api/PersonServicePort.java
@@ -2,23 +2,26 @@ package co.crisi.port.api;
 
 import co.crisi.data.PersonInfo;
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonServicePort {
 
     Long save(PersonInfo personInfo);
 
-    void delete(PersonInfo personInfo);
+    void delete(Long id);
 
     List<PersonInfo> findAll();
 
-    List<PersonInfo> findOnlyMotherFamily(Long personId);
+    PersonInfo findOnlyMotherFamily(Long personId);
 
-    List<PersonInfo> findOnlyFatherFamily(Long personId);
+    PersonInfo findOnlyFatherFamily(Long personId);
 
     List<PersonInfo> findChildren(Long personId);
 
     List<PersonInfo> findParents(Long personId);
 
     Long update(Long id, PersonInfo newPersonInfo);
+
+    PersonInfo findById(Long personId);
 
 }
---
+++ domain/src/main/java/co/crisi/service/PersonServicePortImpl.java
@@ -0,0 +1,87 @@
+package co.crisi.service;
+
+import co.crisi.data.PersonInfo;
+import co.crisi.exception.NoPeopleFoundToDeleteException;
+import co.crisi.exception.ParentNotFoundException;
+import co.crisi.exception.PersonNotFoundException;
+import co.crisi.port.api.PersonServicePort;
+import co.crisi.port.spi.PersonPersistencePort;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.val;
+
+@AllArgsConstructor
+public class PersonServicePortImpl implements PersonServicePort {
+
+    private PersonPersistencePort personPersistencePort;
+
+    @Override
+    public Long save(PersonInfo personInfo) {
+        return personPersistencePort.save(personInfo)
+                .getId();
+    }
+
+    @Override
+    public void delete(Long id) {
+        val eliminatedRows = personPersistencePort.deleteById(id);
+        if (eliminatedRows == 0) {
+            throw new NoPeopleFoundToDeleteException(id);
+        }
+    }
+
+    @Override
+    public List<PersonInfo> findAll() {
+        return personPersistencePort.findAll();
+    }
+
+    @Override
+    public PersonInfo findOnlyMotherFamily(Long personId) {
+        return Optional.ofNullable(findById(personId))
+                .map(PersonInfo::getMother)
+                .orElseThrow(
+                        () -> new ParentNotFoundException(String.format("The mother of %d was not found", personId)));
+    }
+
+    @Override
+    public PersonInfo findOnlyFatherFamily(Long personId) {
+        return Optional.ofNullable(findById(personId))
+                .map(PersonInfo::getFather)
+                .orElseThrow(
+                        () -> new ParentNotFoundException(String.format("The father of %d was not found", personId)));
+    }
+
+    @Override
+    public List<PersonInfo> findChildren(Long personId) {
+        var children = personPersistencePort.findAllByMotherId(personId);
+        children.addAll(personPersistencePort.findAllByFatherId(personId));
+        return children;
+    }
+
+    @Override
+    public List<PersonInfo> findParents(Long personId) {
+        val person = findById(personId);
+        var parents = new ArrayList<PersonInfo>();
+        if (Objects.nonNull(person.getMother())) {
+            parents.add(person.getMother());
+        } else if (Objects.nonNull(person.getFather())) {
+            parents.add(person.getFather());
+        }
+        return parents;
+    }
+
+    @Override
+    public Long update(Long id, PersonInfo newPersonInfo) {
+        return personPersistencePort.update(id, newPersonInfo).getId();
+    }
+
+    @Override
+    public PersonInfo findById(Long personId) {
+        return personPersistencePort.findById(personId)
+                .orElseThrow(() -> new PersonNotFoundException(personId));
+    }
+
+
+}
