5f8d0f6b7650252e7e88a6ed5ea70722540226c7
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-25 14:11:41 -0500

    Added luncher

--- ms-launcher/src/main/java/co/crisi/FamilyGenogramApplication.java
+++ ms-launcher/src/main/java/co/crisi/FamilyGenogramApplication.java
@@ -3,7 +3,7 @@ package co.crisi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "co.crisi")
 public class FamilyGenogramApplication {
 
     public static void main(String[] args) {
